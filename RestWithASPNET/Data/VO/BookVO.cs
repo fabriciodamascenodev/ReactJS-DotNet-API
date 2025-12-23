24e38f9244f09ea7ab0499e50679b73b1a46e37a
Fabricio M Damasceno <fabricio.damasceno@proton.me>
2025-12-22 21:48:20 -0300

    build: setup development environment and fix initial configuration
    - Enable multiple launch profiles (IIS, Kestrel, Docker)
    - Fix URI bug in Swagger configuration
    - Add Docker support
    - Update default user credentials

---
+++ RestWithASPNET/Data/VO/BookVO.cs
@@ -0,0 +1,20 @@
+﻿using RestWithASPNET.Hypermedia;
+using RestWithASPNET.Hypermedia.Abstract;
+
+namespace RestWithASPNET.Data.VO
+{
+    public class BookVO : ISupportsHyperMedia
+    {
+        public long Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public decimal Price { get; set; }
+
+        public DateTime LaunchDate { get; set; }
+        public List<HyperMediaLink> Links { get; set; } = new List<HyperMediaLink>();
+
+    }
+}
